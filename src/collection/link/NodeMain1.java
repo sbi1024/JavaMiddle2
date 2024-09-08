13037aa5589fd7734c1b8b424cb4dfd981c50693
seoboin <sbi1024@naver.com>
2024-09-08 22:29:30 +0900

    섹션5 컬렉션 프레임워크-LinkedList 완료

---
+++ src/collection/link/NodeMain1.java
@@ -0,0 +1,24 @@
+package collection.link;
+
+public class NodeMain1 {
+    public static void main(String[] args) {
+        // 노드 생성하고 연결하기
+
+        Node first = new Node("A");
+        first.next = new Node("B");
+        first.next.next = new Node("C");
+
+        System.out.println("모든 노드 탐색하기");
+
+        System.out.println("first.item = " + first.item);
+        System.out.println("first.next.item = " + first.next.item);
+        System.out.println("first.next.next.item = " + first.next.next.item);
+
+        Node x = first;
+        while (x != null) {
+            System.out.println(x.item);
+            System.out.println(x.next);
+            x = x.next;
+        }
+    }
+}
